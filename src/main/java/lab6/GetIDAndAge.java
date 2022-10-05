2ac2a386e8de20d214b251d8f1cc28fb04cbf4a3
RyanDude <guotajun@gmail.com>
2022-10-04 23:27:37 -0400

    .\README.MD

--- src/main/java/lab6/GetIDAndAge.java
+++ src/main/java/lab6/GetIDAndAge.java
@@ -9,32 +9,35 @@ public class GetIDAndAge
     public static void main(String[] args)
     {
       int id,age;
-      boolean bSTOP=false;
       Scanner keyboard = new Scanner(System.in);
-      while(!bSTOP) {
-         //your try block:
-		 //display message using showStatus() to input ID in range 0..999
+      while(true) {
           try{
               showStatus("pls input age");
               String a = keyboard.nextLine();
               age = Integer.parseInt(a);
               if(age < 0 || age > 999 ){
                   throw new DataEntryException("Number out of range");
-              }else if(age == 0){return;}
+              }else if(age == 0){
+                  keyboard.close();
+                  return;
+              }
 
               showStatus("pls input id");
-              String b = keyboard.nextLine();
-              id = Integer.parseInt(b);
+              a = keyboard.nextLine();
+              id = Integer.parseInt(a);
               if(id < 0 || id > 999 ){
                   throw new DataEntryException("Number out of range");
-              }else if(id == 0){return;}
+              }else if(id == 0){
+                  keyboard.close();
+                  return;
+              }
           }
           catch (InputMismatchException e){e.printStackTrace();}
           catch (DataEntryException e){e.printStackTrace();}
 		 //catch block for 'DataEntryException'
 		 //catch block for 'InputMismatchException'
       }
-	  keyboard.close();
+
 	  //dispay thank you message
     }
     public static void showStatus(String msg)
